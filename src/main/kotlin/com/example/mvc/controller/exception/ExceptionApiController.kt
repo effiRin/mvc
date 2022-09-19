69015dc00fd6168ecd3d61060275996c0974b8b2
effiRin <effirin8@gmail.com>
2022-09-20 00:03:26 +0900

    ControllerAdvice 예외 처리 실습_ 220920

--- src/main/kotlin/com/example/mvc/controller/exception/ExceptionApiController.kt
+++ src/main/kotlin/com/example/mvc/controller/exception/ExceptionApiController.kt
@@ -10,6 +10,8 @@ import javax.validation.constraints.Size
 import com.example.mvc.model.http.Error
 import com.example.mvc.model.http.ErrorResponse
 import com.example.mvc.model.http.UserRequest
+import org.springframework.validation.FieldError
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.*
 import java.time.LocalDateTime
 import javax.servlet.http.HttpServletRequest
@@ -49,9 +51,49 @@ class ExceptionApiController {
     }
 
     @PostMapping("")
-    fun post(@Valid @RequestBody userRequest: UserRequest): UserRequest{
+    fun post(@Valid @RequestBody userRequest: UserRequest): UserRequest {
         println(userRequest)
         return userRequest
+    }
+
+    @ExceptionHandler(value = [MethodArgumentNotValidException::class])
+    fun methodArgumentNotValidException(
+        e: MethodArgumentNotValidException,
+        request: HttpServletRequest
+    ): ResponseEntity<ErrorResponse> {
+        val errorList = mutableListOf<Error>()
+
+        e.bindingResult.allErrors.forEach { errorObject ->
+            val errors = Error().apply {
+
+                //		val field = it as FieldError   // 여기서 it이 objectError인데 FieldError로 형변환
+                //		this.field = field.field
+                //		this.message = it.defaultMessage
+                //		this.value = it.
+
+                val field = errorObject as FieldError
+                this.field = field.field
+                this.message = errorObject.defaultMessage
+                this.value = errorObject.rejectedValue
+
+                // 앞서 형변환을 해줬기 때문에 it으로 받는 것에 한계가 있어서 errorObject로 이름을 바꿔줌.
+            }
+            errorList.add(errors)
+        }//forEach
+
+        // 2. ErrorResponse 만들기
+        val errorResponse = ErrorResponse().apply {
+            this.resultCode = "FAIL"
+            this.httpStatus = HttpStatus.BAD_REQUEST.value().toString()
+            this.httpMethod = request.method
+            this.message = "요처엥 에러가 발생했습니다"
+            this.path = request.requestURI
+            this.timestamp = LocalDateTime.now()
+            this.errors = errorList
+        }//apply
+
+        //3. ResponseEntity로 반환
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse)
     }
 
     @ExceptionHandler(value = [ConstraintViolationException::class])
@@ -77,12 +119,14 @@ class ExceptionApiController {
         val errorResponse = ErrorResponse().apply {
             this.resultCode = "FAIL"
             this.httpStatus = HttpStatus.BAD_REQUEST.value().toString()
+            this.httpMethod = request.method
             this.message = "요처엥 에러가 발생했습니다"
             this.path = request.requestURI
-//            this.timestamp = LocalDateTime.now()
+            this.timestamp = LocalDateTime.now()
             this.errors = errorList
         }//apply
 
+        //3. ResponseEntity로 반환
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse)
     }
 }
